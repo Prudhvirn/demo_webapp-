59d530807ecf366676a63f8fa44e579b2856ae6e
Prudhvi Posimsetty <vposimsetty@deloitte.com>
2020-04-19 16:17:08 +0530

    updated message

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace demo_webapp_
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure!!!");
             });
         }
     }
